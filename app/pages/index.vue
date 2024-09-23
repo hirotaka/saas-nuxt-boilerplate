29d7c1ddc444d4e275352c4c22a7f88c9db90f1b
Hirotaka Mizutani <52546+hirotaka@users.noreply.github.com>
2024-09-23 17:55:49 +0900

    feat: add tailwindcss module

---
+++ app/pages/index.vue
@@ -0,0 +1,3 @@
+<template>
+  <div class="text-red-500">Hello, Nuxt.js!</div>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,16 +1,13 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: "2024-08-03",
+  srcDir: "app",
   devtools: { enabled: true },
-  modules: ["@nuxt/eslint", "@nuxtjs/storybook"],
+  modules: ["@nuxt/eslint", "@nuxtjs/storybook", "@nuxtjs/tailwindcss"],
   vite: {
     optimizeDeps: {
       include: ["jsdoc-type-pratt-parser"],
     },
-  },
-  storybook: {
-    // Very verbose logs for debugging
-    logLevel: Number.POSITIVE_INFINITY,
   },
 })
 
